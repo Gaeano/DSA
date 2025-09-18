fa807e5a3dde9d2a48983f13e79ccd0abaebe1e3
Gaeano <francesannagaeamutia@gmail.com>
2025-09-18 23:45:35 +0800

    initheap, alloc, dealloc functions added

--- FIRST EXAM EXERCISES/knowledgeBase/cursorBased.h
+++ FIRST EXAM EXERCISES/knowledgeBase/cursorBased.h
@@ -22,9 +22,36 @@ typedef struct{
 }VHeap;
 
 typedef int List;
+void initHeap(VHeap* heap);
+int allocSpace (VHeap *heap);
+void deallocSpace(VHeap *heap, int idx);
 
+void initHeap(VHeap* heap){
+    int i;
+    for (i = 0; i < MAX - 1; i++){
+        heap->H[i].next = i+1;
+    }
+    heap->H[i].next = -1;
+    heap->avail = 0;
+}
 
+int allocSpace (VHeap *heap){
+    int retVal;
 
+    if(heap->avail != -1){
+        retVal = heap->avail;
+        heap->avail = heap->H[retVal].next;
+    }
+
+    return retVal;
+}
+
+void deallocSpace(VHeap *heap, int idx){
+    if (idx != -1 && idx < MAX ){
+        heap->H[idx].next = heap->avail;
+        heap->avail = idx;
+    }
+}
 
 
 #endif
