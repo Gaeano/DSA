a9b8cc4c879e7b66255e8fa507216f1d08f70102
Gaeano <francesannagaeamutia@gmail.com>
2025-09-19 11:06:36 +0800

    cursor based functionality implemented

--- FIRST EXAM EXERCISES/knowledgeBase/cursorBased.h
+++ FIRST EXAM EXERCISES/knowledgeBase/cursorBased.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #ifndef cursorBased_H
 #define cursorBased_H
 #define MAX 20
@@ -12,7 +13,7 @@ typedef struct{
 
 
 typedef struct{
-    Article art;
+    Article* art;
     int next;
 }Heapspace;
 
@@ -25,6 +26,11 @@ typedef int List;
 void initHeap(VHeap* heap);
 int allocSpace (VHeap *heap);
 void deallocSpace(VHeap *heap, int idx);
+void insertPos(VHeap * heap, List* L, int pos, Article* data);
+void display(VHeap* heap, List* L);
+void locate (VHeap* heap, List* L, int id);
+void deleteArticle(VHeap * heap, List *L, int id);
+bool getIDTrue(VHeap* heap, List* L, int id);
 
 void initHeap(VHeap* heap){
     int i;
@@ -50,6 +56,77 @@ void deallocSpace(VHeap *heap, int idx){
     if (idx != -1 && idx < MAX ){
         heap->H[idx].next = heap->avail;
         heap->avail = idx;
+    }
+}
+
+void insertPos(VHeap * heap, List* L, int pos, Article* data){
+    List temp = allocSpace(heap);
+    List* trav;
+    if (pos == -1){
+        for (trav = L; *trav != -1; trav = &heap->H[*trav].next){
+            
+        }
+
+        heap->H[temp].art = data;
+        heap->H[temp].next = -1;
+        *trav = temp;
+    } else {
+        for (trav = L; *trav != -1 && *trav < pos - 1; trav = &heap->H[*trav].next){
+        }
+        heap->H[temp].art = data;
+        heap->H[temp].next = *trav;
+        *trav = temp;
+
+    }
+
+       
+        printf("Article with id: %d added to position %d\n", data->id, pos++);
+    
+}
+
+void display(VHeap* heap, List* L){
+    List *trav;
+    printf("---------------------------------------\n");
+    printf("%-5s %-8s %-10s\n", "ID", "Title", "Description");
+    for (trav = L; *trav != -1; trav = &heap->H[*trav].next){
+        printf("%-5d %-8s %-10s\n", heap->H[*trav].art->id, heap->H[*trav].art->title, heap->H[*trav].art->content);
+    }
+}
+
+bool getIDTrue(VHeap* heap, List* L, int id){
+    return heap->H[*L].art->id != id;
+}
+
+void locate (VHeap* heap, List* L, int id){
+    List *trav;
+
+    for (trav = L; *trav != -1 && getIDTrue(heap, trav, id); trav = &heap->H[*trav].next){
+
+    }
+    if (*trav != -1){
+        printf("Article Found\nID: %d Title: %s Description: %s\n", heap->H[*trav].art->id, heap->H[*trav].art->title, heap->H[*trav].art->content);    
+    } else {
+        printf("Article not found\n");
+    }
+
+
+}
+
+void deleteArticle(VHeap * heap, List *L, int id){
+    List *trav;
+    List prev = -1;
+    for (trav = L; *trav != -1 && getIDTrue(heap, trav, id); trav = &heap->H[*trav].next){
+        prev = *trav;
+    }
+
+    if (*trav != -1){
+        List tempTodelete = *trav;
+        heap->H[prev].next = heap->H[*trav].next;
+        deallocSpace(heap, tempTodelete);
+
+        printf("Article with id: %d successfully deleted\n", id);
+    } else {
+        printf("Article to delete not found\n");
     }
 }
 
--- FIRST EXAM EXERCISES/knowledgeBase/knowledge.c
+++ FIRST EXAM EXERCISES/knowledgeBase/knowledge.c
@@ -1,3 +1,83 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "cursorBased.h"
+
+
+int main (){
+    VHeap heap;
+    List L = -1;
+
+    initHeap(&heap);
+
+   while (1){
+    
+    int choice;
+    printf("---------------------------------------\n");
+    printf("1. Insert Article\n2.View List of Articles\n3.Search for an article\n4.Delete an article\npress -1 to exit\nchoice: ");
+    scanf("%d", &choice);
+    getchar();
+
+
+    if (choice == -1){
+        break;
+    }
+
+    switch(choice){
+        case 1:
+        int i;
+        int pos; 
+        int id;
+        i++;
+        Article* art = (Article*) malloc (sizeof(Article));
+        printf("Enter Title: ");
+        scanf("%[^\n]s", &art->title);
+        // fgets(art->title, sizeof(art->title), stdin);
+        getchar();
+
+        // art->title[strcspn(art->title, "\n")] = '\0';
+
+
+        printf("Enter Description: ");
+        scanf("%[^\n]s", &art->content);
+        // fgets(art->content, sizeof(art->content), stdin);
+        getchar();
+        // art->content[strcspn(art->content, "\n")] = '\0';
+        
+
+        art->id = i;
+        printf("Enter position to insert: ");
+        scanf("%d", &pos);
+        getchar();
+
+        
+
+
+        insertPos(&heap, &L, pos, art);
+        break;
+
+        case 2:
+        display(&heap, &L);
+        break;
+
+        case 3:
+        printf("Enter id of the article to find: ");
+        scanf("%d", &id);
+        locate(&heap, &L, id);
+        break;
+
+        case 4:
+        printf("Enter id of the article to delete: ");
+        scanf("%d", &id);
+
+        deleteArticle(&heap, &L, id);
+        break;
+
+        default:
+        printf("Wrong choice!");
+
+    } 
+
+   }
+
+}
